c0df515c5496694167f814d9e69bbf52866211e5
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-09 15:58:49 +0530

    updating with usecase two

--- MaxMin.java
+++ MaxMin.java
@@ -1,5 +1,22 @@
 package practiceday13problem.PracticeProb13;
 
+import java.util.Arrays;
+
 public class MaxMin {
 
+    // UC 1, UC 2, UC 3, Refactor 1: Generic Method for 3 variables
+    public static <T extends Comparable<T>> T max(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
+        return max;
+    }
+
+    
+    public static void main(String[] args) {
+        System.out.println("=== UC 1: Integers ===");
+        System.out.println(max(5, 3, 2)); // Max at 1st
+        System.out.println(max(3, 7, 2)); // Max at 2nd
+        System.out.println(max(3, 2, 9)); // Max at 3rd
+    }
 }
